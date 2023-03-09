e13fc52487936453804479a24b9695fa73f58236
Chaslinux <chaslinux@gmail.com>
2023-03-09 12:26:23 -0500

    Using xargs to get rid of spaces. Manufacturer is now aligned correctly. Parskip added for spacing issues.

--- findmonitor.sh
+++ findmonitor.sh
@@ -27,58 +27,61 @@ if [ ! -f /home/$USER/Desktop/monitor.tex ]; then
 	echo "\documentclass{article}" >> /home/$USER/Desktop/monitor.tex
 	echo "\usepackage{blindtext}" >> /home/$USER/Desktop/monitor.tex
 	echo "\usepackage[paperheight=4.0in,paperwidth=8.0in,margin=0.25in,heightrounded,showframe]{geometry}" >> /home/$USER/Desktop/monitor.tex
+	echo "\usepackage{parskip}" >> /home/$USER/Desktop/monitor.tex
 	echo "\begin{document}" >> /home/$USER/Desktop/monitor.tex
 fi
 
-LONGMFG=$(xrandr --verbose | edid-decode | grep "Manufacturer" | cut -c 19-)
-SHORTMFG=$(echo $LONGMFG | cut -c -3)
-xrandr --verbose | edid-decode | grep "Manufacturer" | cut -c 5- >> /home/$USER/Desktop/monitor.tex
+LONGMFG=$(xrandr --verbose | edid-decode | grep "Manufacturer" | xargs | cut -c 14-)
+SHORTMFG=$(echo $LONGMFG | cut -c -3 | xargs)
+xrandr --verbose | edid-decode | grep "Manufacturer" | xargs >> /home/$USER/Desktop/monitor.tex
 echo "\newline" >> /home/$USER/Desktop/monitor.tex
 xrandr --verbose | edid-decode | grep "Product Name" >> /home/$USER/Desktop/monitor.tex
 echo "\newline" >> /home/$USER/Desktop/monitor.tex
-xrandr --verbose | edid-decode | grep "Serial Number:" >> /home/$USER/Desktop/monitor.tex
+xrandr --verbose | edid-decode | grep "Product Serial Number:" >> /home/$USER/Desktop/monitor.tex
 echo "\newline" >> /home/$USER/Desktop/monitor.tex
-xrandr | grep " connected" | cut -c 18-  >> /home/$USER/Desktop/monitor.tex
+# xrandr | grep " connected" | cut -c 18-  >> /home/$USER/Desktop/monitor.tex
+#xrandr | grep " connected" | xargs >> /home/$USER/Desktop/monitor.tex
+xrandr | grep " connected" |sed -n -e 's/^.*connected //p' | xargs >> /home/$USER/Desktop/monitor.tex
 echo "\newline" >> /home/$USER/Desktop/monitor.tex
 case $SHORTMFG in
 	HWP)
-		echo -n "HP"  >> /home/$USER/Desktop/monitor.tex
+		echo -n "HP"  >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	GSM)
-		echo -n "LG"  >> /home/$USER/Desktop/monitor.tex
+		echo -n "LG"  >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	SAM)
-		echo -n "Samsung" >> /home/$USER/Desktop/monitor.tex
+		echo -n "Samsung" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	VSC)
-		echo -n "ViewSonic" >> /home/$USER/Desktop/monitor.tex
+		echo -n "ViewSonic" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	LEN)
-		echo -n "Lenovo" >> /home/$USER/Desktop/monitor.tex
+		echo -n "Lenovo" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	VIT)
-		echo -n "3M" >> /home/$USER/Desktop/monitor.tex
+		echo -n "3M" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	DEL)
-		echo -n "Dell" >> /home/$USER/Desktop/monitor.tex
+		echo -n "Dell" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	NEC)
-		echo -n "NEC" >> /home/$USER/Desktop/monitor.tex
+		echo -n "NEC" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	ACI)
-		echo -n "ASUS" >> /home/$USER/Desktop/monitor.tex
+		echo -n "ASUS" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	AOC)
-		echo -n "AOC" >> /home/$USER/Desktop/monitor.tex
+		echo -n "AOC" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	ACR)
-		echo -n "Acer" >> /home/$USER/Desktop/monitor.tex
+		echo -n "Acer" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	BNQ)
-		echo -n "BenQ" >> /home/$USER/Desktop/monitor.tex
+		echo -n "BenQ" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 	*)
-		echo "I don't know this monitor" >> /home/$USER/Desktop/monitor.tex
+		echo "I don't know this monitor" >> /home/$USER/Desktop/monitor.tex >> /home/$USER/Desktop/monitor.tex
 		;;
 esac
 echo -ne "\n"  >> /home/$USER/Desktop/monitor.tex
